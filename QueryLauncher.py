4f3bcb877af0cd5a817ab7a230cb11fa81039274
network-performance-repo <61080985+network-performance-repo@users.noreply.github.com>
2021-01-02 16:02:30 +0330

    Update QueryLauncher.py

--- QueryLauncher.py
+++ QueryLauncher.py
@@ -1,6 +1,8 @@
 import DBLoader as dbl
 import pandas as pd
 import cx_Oracle
+import yaml as yml
+
 
 def OutputTypeHandler(cursor, name, defaultType, size, precision, scale):
     if defaultType == cx_Oracle.CLOB:
@@ -12,7 +14,10 @@ def fetchTheQueries(oraconn):
     oraconn.outputtypehandler = OutputTypeHandler
 
     try:
-        cur.execute("select tech,vendor,target,query_ from global_gsm.cellcfg_query_repository")
+        cur.execute("select tech,vendor,target,query_ from global_gsm.cellcfg_query_repository"
+                    " where vendor || '_' || tech || '_' || target <> 'Ericsson_2G_GIS'"
+                    " order by target,tech,vendor") # there is an encoding issue
+        # with Ericsson_2G_GIS data which I cannot resolve yet.
     except cx_Oracle.DatabaseError as e:
         print("There is a problem in fetching the queries list.", e)
 
@@ -37,15 +42,25 @@ def saveTheResult(query, df_result, writer):
     df_result.to_excel(writer, sheet_name=tech + '_' + vendor + '_' + target)
 
 def appendTheSummary(query, df_result, arr_result_summary_data):
-    issuedRows = len(df_result.index)
     tech, vendor, target, query_ = query
+
+    importantIssues = 0
+    otherIssues = 0
+    if 'Count' not in target:
+        importantIssues = df_result['COUNT_IMPORTANT_ISSUES'].sum()
+        otherIssues = df_result['COUNT_OTHER_ISSUES'].sum()
+
     testCaption = 'Rows with issue in ' + tech + '_' + vendor + '_' + target
-    arr_result_summary_data.append([testCaption, issuedRows])
+    arr_result_summary_data.append([testCaption, otherIssues + importantIssues, importantIssues, otherIssues])
 
 def launchTheQueries(oraconn):
     queries = fetchTheQueries(oraconn)
 
-    writer = pd.ExcelWriter(r'E:\data\cellcfg\20200128\cellcfg_report_20200128.xlsx')
+    config = yml.safe_load(open("CellcfgVerifier.yml"))
+    outputDirectory = config['report']['directory']
+    outputReportName = config['report']['filename']
+
+    writer = pd.ExcelWriter(outputDirectory + '\\' + outputReportName)
 
     arr_result_summary_data = [] # this is to collect summary of test result in a loop and later to be written to
         # first excel sheet
@@ -65,8 +80,7 @@ def launchTheQueries(oraconn):
 
         appendTheSummary(query, df_result, arr_result_summary_data)
 
-
-    df_result_summary = pd.DataFrame(data=arr_result_summary_data, columns=['Test', 'Result'])
+    df_result_summary = pd.DataFrame(data=arr_result_summary_data, columns=['Test', 'All Issues', 'Important Issues','Other Issues'])
     df_result_summary.to_excel(writer, sheet_name='Test Result Summary')
 
     writer.save()
